ef6fd874c9df67d212f1b65b145b50cce1949acd
Simon Folkerts <simfolkerts@gmail.com>
2020-08-06 09:39:23 +1200

    infinitely improved via removing header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <MyHeader />
     <router-view></router-view>
-    <MyFooter />
   </div>
 </template>
 
 <script>
-import MyHeader from "./components/Header";
-import MyFooter from "./components/Footer";
 
 export default {
   name: "App",
   components: {
-    MyHeader,
-    MyFooter
   }
 };
 </script>
